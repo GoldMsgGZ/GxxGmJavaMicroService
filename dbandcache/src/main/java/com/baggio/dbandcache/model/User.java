00fed67fcf419925a0de2556ca775a563921d144
baggiowangyu <baggiowangyu@gmail.com>
2019-06-03 23:51:18 +0800

    创建Spring Boot数据库项目

---
+++ dbandcache/src/main/java/com/baggio/dbandcache/model/User.java
@@ -0,0 +1,12 @@
+package com.baggio.dbandcache.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private Long id;
+    private String name;
+    private String phone;
+}
