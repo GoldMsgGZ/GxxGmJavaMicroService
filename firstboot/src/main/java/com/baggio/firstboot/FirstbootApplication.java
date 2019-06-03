f2134289e13c889dc8ee168fbdb138933550d6da
baggiowangyu <baggiowangyu@gmail.com>
2019-06-03 14:02:48 +0800

    完成Swagger2集成范例

--- firstboot/src/main/java/com/baggio/firstboot/FirstbootApplication.java
+++ firstboot/src/main/java/com/baggio/firstboot/FirstbootApplication.java
@@ -2,6 +2,7 @@ package com.baggio.firstboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @SpringBootApplication
@@ -11,6 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * - @ComponentScan:此注解用于扫描类，其作用类似于Spring中的<context:component-scan></context:component-scan>标签
  */
 @SpringBootApplication
+
+/**
+ * @EnableSwagger2
+ * 这个注解用于启动Swagger2
+ */
+@EnableSwagger2
 public class FirstbootApplication {
 
     public static void main(String[] args) {
