cfdf8b7191612f74043dab5a089895bc6050c6c3
baggiowangyu <baggiowangyu@gmail.com>
2019-06-11 01:50:27 +0800

    Consul服务发现工程B

---
+++ ConsulDemo/consulservicea/src/main/java/com/baggio/consulservicea/ConsulserviceaApplication.java
@@ -0,0 +1,13 @@
+package com.baggio.consulservicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulserviceaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulserviceaApplication.class, args);
+    }
+
+}
